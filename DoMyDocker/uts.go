ec791c9ad82c7c6aca07c5d12a054e8cf52f173e
aniccaa <ligang108@huawei.com>
2019-11-24 13:13:33 +0800

    daily commit.1124

--- DoMyDocker/uts.go
+++ DoMyDocker/uts.go
@@ -1,1 +1,30 @@
-package DoMyDocker
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	//cmd1 := exec.Cmd{}
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUSER,
+	}
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
